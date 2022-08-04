53aeafdfb5fc8bfeafe96e552e4796886a205079
Maria Antonia <maria@savesolarcorporation.com>
2022-08-04 15:13:44 -0500

    Deply commit

--- src/containers/StatusDashboard.js
+++ src/containers/StatusDashboard.js
@@ -10,7 +10,7 @@ const StatusDashboard = () => {
 	return(
 		<div className={styles.dashboard}>
 			<div className={styles.dashboardTitle}>Status Dashboard</div>
-			<Grid container spacing={{ sm: 2, md: 3, lg:4 }} columns={{ xs: 1, sm: 2, md: 4, lg:6 }}>
+			<Grid container spacing={{ xs:4, sm: 4, md: 4, lg:4 }} columns={{ xs: 1, sm: 2, md: 4, lg:6 }}>
 				{apiInfo.map(({title, apiURL}) => {
 					return(
 						<Grid item key={title} xs={6} sm={4} md={2} lg={1}>
